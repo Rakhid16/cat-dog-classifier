5b41a6c2798cf6299fe6d4fe37b0258f61e9772e
Rakhman Wahid <roploverz@gmail.com>
2019-11-14 12:23:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,25 @@
-from __future__ import division, print_function
-# coding=utf-8
-import sys
-import os
-import glob
-import re
-import numpy as np
+# BUILT-IN LIBRARY UNTUK MENDAPATKAN NAMA DIREKTORI DAN MENGGABUNGKAN 
+from os.path import dirname, join
 
-# Keras
-from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from numpy import expand_dims
 from keras.models import load_model
 from keras.preprocessing import image
 
-# Flask utils
-from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
+from flask import Flask, request, render_template
 
-# Define a flask app
 app = Flask(__name__)
-
-# Model saved with Keras model.save()
-# MODEL_PATH = 'models/your_model.h5'
 
 # Load your trained model
 model = load_model("models/koceng_anjeng.h5")
 model._make_predict_function()          # Necessary
 print('Model loaded. Start serving...')
-
-# You can also use pretrained model from Keras
-# Check https://keras.io/applications/
-#from keras.applications.resnet50 import ResNet50
-#model = 'models/cells.h5'
 print('Model loaded. Check http://127.0.0.1:5000/')
 
 def load_image(img_path, show=False):
-    img = image.load_img(img_path, target_size=(150, 150))
+    img = image.load_img(img_path, target_size=(224, 224))
     img_tensor = image.img_to_array(img)                    # (height, width, channels)
-    img_tensor = np.expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
+    img_tensor = expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
     img_tensor /= 255.                                      # imshow expects values in the range [0, 1]
 
     if show:
@@ -46,27 +29,9 @@ def load_image(img_path, show=False):
 
     return img_tensor
 
-# def model_predict(img_path, model):
-#     img = image.load_img(img_path, target_size=(224, 224))
-
-#     # Preprocessing the image
-#     x = image.img_to_array(img)
-#     # x = np.true_divide(x, 255)
-#     x = np.expand_dims(x, axis=0)
-
-#     # Be careful how your trained model deals with the input
-#     # otherwise, it won't make correct prediction!
-#     x = preprocess_input(x, mode='caffe')
-
-#     preds = model.predict(x)
-#     return preds
-
-
 @app.route('/', methods=['GET'])
 def index():
-    # Main page
     return render_template('index.html')
-
 
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
@@ -75,8 +40,8 @@ def upload():
         f = request.files['image']
 
         # Save the file to ./uploads
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(
+        basepath = dirname(__file__)
+        file_path = join(
             basepath, 'uploads', secure_filename(f.filename))
         f.save(file_path)
 
@@ -84,19 +49,11 @@ def upload():
         new_image = load_image(file_path)
         pred = model.predict(new_image)
 
-        #preds = model_predict(file_path, model)
-
-        # Process your result for human
-        #pred_class = preds.argmax(axis=-1)            # Simple argmax
-        #pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-        result = str(pred[0])               # Convert to string
+        result = str(pred[0])
         return result
     return None
 
 
 if __name__ == '__main__':
-    # app.run(port=5002, debug=True)
-
-    # Serve the app with gevent
     http_server = WSGIServer(('0.0.0.0', 5000), app)
     http_server.serve_forever()
