21d130737e3f554ad8f9e4c94c4062df0e719cec
Rakhman Wahid <roploverz@gmail.com>
2019-11-14 12:35:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
-# BUILT-IN LIBRARY UNTUK MENDAPATKAN NAMA DIREKTORI DAN MENGGABUNGKAN 
+# UNTUK MENDAPATKAN NAMA DIREKTORI DAN MENGGABUNGKAN PATH
 from os.path import dirname, join
 
+# UNTUK MENNAMBAHKAN AXIS, MEMUAT MODEL DAN GAMBAR
 from numpy import expand_dims
 from keras.models import load_model
 from keras.preprocessing import image
 
+# UNTUK BACK-END SITUS
 from werkzeug.utils import secure_filename
 from flask import Flask, request, render_template
 
+# BIAR BISA JALAN WEB'NYA
 app = Flask(__name__)
 
-# Load your trained model
+# MUAT MODEL YANG TELAH ANTUM LATIH
 model = load_model("models/koceng_anjeng.h5")
-model._make_predict_function()          # Necessary
-print('Model loaded. Start serving...')
+model._make_predict_function()
 print('Model loaded. Check http://127.0.0.1:5000/')
 
+# UNTUK MEMUAT/MEMBACA CITRA HASIL UNGGAHAN
 def load_image(img_path, show=False):
     img = image.load_img(img_path, target_size=(224, 224))
-    img_tensor = image.img_to_array(img)                    # (height, width, channels)
-    img_tensor = expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
-    img_tensor /= 255.                                      # imshow expects values in the range [0, 1]
+    img_tensor = image.img_to_array(img)
+    img_tensor = expand_dims(img_tensor, axis=0)         
+    img_tensor /= 255.
 
     if show:
         plt.imshow(img_tensor[0])                           
@@ -36,23 +39,20 @@ def index():
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
-        # Get the file from post request
         f = request.files['image']
-
-        # Save the file to ./uploads
+        
         basepath = dirname(__file__)
         file_path = join(
             basepath, 'uploads', secure_filename(f.filename))
         f.save(file_path)
 
-        # Make prediction
         new_image = load_image(file_path)
         pred = model.predict(new_image)
 
         result = str(pred[0])
         return result
+    
     return None
-
 
 if __name__ == '__main__':
     http_server = WSGIServer(('0.0.0.0', 5000), app)
